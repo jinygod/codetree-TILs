195f45abb6b797e73ff2af77f1da00af73637674
전성진 <103230840+jinygod@users.noreply.github.com>
2023-11-08 10:30:23 +0900

    Title: 특정 위치의 문자 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231108/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] strArr = new String[10];
+        for(int i = 0; i < 10; i++){
+            strArr[i] = sc.next();
+        }
+
+        System.out.print(strArr[1] + " " + strArr[4] + " "+ strArr[7]);
+    }
+}
