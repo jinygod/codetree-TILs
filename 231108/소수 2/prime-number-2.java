53f3899b499bcc5bd40f5d4c0170d6b00d4d214d
전성진 <103230840+jinygod@users.noreply.github.com>
2023-11-08 14:06:46 +0900

    Title: 소수 2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231108/소수 2/prime-number-2.java
+++ 231108/소수 2/prime-number-2.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int cnt = n+1;
+        int cnt =1;
 
-        for(int i = 1; i <= n; i++){
-            if(n % i == 0){
-                cnt--;
+        for(int i =2 ; i <= n; i++){
+            if(n % i != 0){
+                cnt++;
             }
         }
         System.out.print(cnt);
