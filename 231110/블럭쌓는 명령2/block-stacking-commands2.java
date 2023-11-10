c9d80d75093472c3dd1690b42b9ffbe5487688c1
전성진 <103230840+jinygod@users.noreply.github.com>
2023-11-10 10:55:14 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231110/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < K; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            for(int j = A; j <= B; j++){
+                arr[j]++;
+            }
+        }
+        int MAX = 0;
+        for(int i = 0; i < N; i++){
+            if(arr[i] > MAX){
+                MAX = arr[i];
+            }
+        }
+
+        System.out.print(MAX);
+    }
+}
