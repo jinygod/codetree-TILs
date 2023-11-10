a2697f525e83d0ffc7c74d7579f6798c9c8b41a9
전성진 <103230840+jinygod@users.noreply.github.com>
2023-11-10 13:09:51 +0900

    Title: 최대로 겹치는 구간, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 231110/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 231110/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,35 +1,45 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static final int MAX_R = 200;
+    public static final int OFFSET = 100;
+
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_N];
+
+    public static int[] checked = new int[MAX_R + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] arr = new int[100];
+        // 입력
+        n = sc.nextInt();
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] += 100;
+        for(int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
         }
 
-        for(int i = 0; i < n; i++){
-            int x1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            for(int j = x1; j <= x2-1; j++){
-                arr[j]++;
-            }
-        }
-
+        // 구간을 칠해줍니다.
+        // 구간 단위로 진행하는 문제이므로
+        // x2[i]에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = x1[i]; j < x2[i]; j++)
+                checked[j]++;
+        
+        // 최댓값을 구합니다.
         int max = 0;
-
-        for(int i = 0; i < n; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
-        }
-
-        System.out.print(max - 100);
-
-
-
+        for(int i = 0; i <= MAX_R; i++)
+            if(checked[i] > max)
+                max = checked[i];
+        
+        // 출력
+        System.out.print(max);
     }
 }
