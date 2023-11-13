c6883ba6fa544a2cf057c243e548a43273fa15cf
전성진 <103230840+jinygod@users.noreply.github.com>
2023-11-13 09:39:59 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231113/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] date = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
+    
+    public static boolean isExist(int M, int D){
+        if(D <= date[M]){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        if(isExist(M,D)){
+            System.out.print("Yes");
+        }
+        else{
+            System.out.print("No");
+        }
+
+
+    }
+}
