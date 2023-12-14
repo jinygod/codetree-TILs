671e92ca728a733971fa5ba8f42adbcc9c01b254
전성진 <103230840+jinygod@users.noreply.github.com>
2023-12-14 10:52:44 +0900

    Title: 특정 위치의 문자, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231214/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char [] arr=new char[]{'L','E','B','R','O','S'};
+        char a = sc.next().charAt(0);
+        int x = -1;
+        for (int i =0; i<6;i++){
+            if(arr[i]==a){
+                x=i;
+            }
+        }
+        if (x==-1){
+            System.out.println("None");
+        }else{
+            System.out.println(x);
+        }
+        
+
+    }
+}
