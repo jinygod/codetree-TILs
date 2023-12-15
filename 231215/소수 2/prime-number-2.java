1f028ab7afebce6c667010ddc9de61f2295ffc7a
전성진 <103230840+jinygod@users.noreply.github.com>
2023-12-15 11:15:59 +0900

    Title: 소수 2, Time: 2878ms, Memory: 10MB, Status: Passed - CodeTree

--- 231215/소수 2/prime-number-2.java
+++ 231215/소수 2/prime-number-2.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean isPrime(int num){
-        if(num == 2){
-            return true;
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
         }
 
-        for(int i = 1; i < num; i++){
-            if(num % i == 0){
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
         return true;
-        
     }
 
     public static void main(String[] args) {
@@ -20,10 +19,11 @@ public class Main {
 
         int n = sc.nextInt();
         int cnt = 0;
-        for(int i = 1; i < n; i++){
-        if(isPrime(i)){
-            cnt++;
-        }
+
+        for (int i = 2; i <= n; i++) { // 시작값을 2로 수정하고 범위를 <= n으로 수정
+            if (isPrime(i)) {
+                cnt++;
+            }
         }
         System.out.print(cnt);
     }
