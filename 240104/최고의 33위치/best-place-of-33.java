7467210eee82735c123b67d906dad245a5597e94
전성진 <103230840+jinygod@users.noreply.github.com>
2024-01-04 13:12:35 +0900

    Title: 최고의 33위치, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240104/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // N 입력 받기, 격자선언
+        int N = sc.nextInt();
+        int[][] arr = new int[N][N];
+
+        // N크기의 격자 입력받기
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int cnt = 0;
+
+        for(int row = 0; row < N; row++){
+            for(int col = 0; col < N; col++){
+                if(col > N){
+                    continue;
+                }
+
+                if(arr[row][col] == 1){
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.print(cnt);
+
+        
+    }
+}
