32920624726ab047744131853f3ff88d613b6840
전성진 <103230840+jinygod@users.noreply.github.com>
2024-05-30 15:04:01 +0900

    Title: 특정 위치의 숫자, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240530/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0;
+
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        sum += arr[2];
+        sum += arr[4];
+        sum += arr[9];
+
+        System.out.print(sum);
+
+    }
+}
