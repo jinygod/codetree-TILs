6db204640aad8fc7d2c2d148fb47bcea8597f11d
전성진 <103230840+jinygod@users.noreply.github.com>
2024-05-31 16:01:18 +0900

    Title: 십의 자리 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240531/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] input = new int[100];
+        int[] countArr = new int[9];
+        int leng = 0;
+
+        for(int i = 0; i < input.length; i++){
+            input[i] = sc.nextInt();
+            if(input[i] == 0){
+                leng = i;
+                break;
+            }
+        }
+
+        for(int i = 0; i < leng; i++){
+            countArr[(input[i] / 10)-1]++;
+        }
+
+        for(int i = 1; i <= countArr.length; i++){
+            System.out.println(i + " - " + countArr[i - 1]);
+        }
+    }
+}
