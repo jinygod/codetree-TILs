ddd4461b57285697ed22d7f064f0d3871ef25744
전성진 <103230840+jinygod@users.noreply.github.com>
2024-05-31 11:06:04 +0900

    Title: 개발자의 공놀이, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240531/개발자의 공놀이/developer's-ball-game.java
+++ 240531/개발자의 공놀이/developer's-ball-game.java
@@ -1,16 +1,31 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] devArr = new int[n];
+        int[] devArr = new int[n +2 ];
+        int order = 0;
+        int ball = 0;
 
         for(int i = 0; i < n; i++){
             devArr[i] = sc.nextInt();
         }
 
-        System.out.print(2);
+        Arrays.sort(devArr);
+
+        for(int i = 2; i < n; i++){
+            if(devArr[i + 1] - devArr[i] > devArr[i + 2] - devArr[i + 1]){
+            } 
+            else if(devArr[i + 1] - devArr[i] < devArr[i + 2] - devArr[i + 1]){
+            //    System.out.println(devArr[i]);
+                ball++;
+                order++;
+            }
+        }
+
+        System.out.print(ball);
     }
 }
